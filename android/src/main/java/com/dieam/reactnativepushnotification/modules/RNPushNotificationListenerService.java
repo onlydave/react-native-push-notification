aea8108f46ce7d702744e9ee70e4c334c1bf92ee
Dave Bennett <dave@primedcanvasmedia.com>
2018-11-06 15:48:59 +0000

    Always show notifications

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -107,11 +107,11 @@ public class RNPushNotificationListenerService extends GcmListenerService {
 
         Log.v(LOG_TAG, "sendNotification: " + bundle);
 
-        if (!isForeground) {
+        // if (!isForeground) {
             Application applicationContext = (Application) context.getApplicationContext();
             RNPushNotificationHelper pushNotificationHelper = new RNPushNotificationHelper(applicationContext);
             pushNotificationHelper.sendToNotificationCentre(bundle);
-        }
+        // }
     }
 
     private boolean isApplicationInForeground() {
